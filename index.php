a0eb2677ae346e52b250d107bf138ea7e9631c94
nkenion615 <nkenion615@mail.cfcc.edu>
2025-09-18 22:56:02 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Building a Database with Stored Procedures</title>
+    <link rel="stylesheet" href="styles/main.css">
+</head>
+<body>
+    <h1>Building a Database with Stored Procedures</h1>
+    <p>Najee Kenion</p>
+    <p>September 18, 2025</p>
+</body>
+</html>
